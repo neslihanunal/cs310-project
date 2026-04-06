8ec651d67b86a97b476ee0613d4aa1f6f4a154b1
Neslihan Unal <neslihan.unal@sabanciuniv.edu>
2026-04-06 14:30:04 +0300

    Refactor: Clean up outdated files and establish precise UI directory structure for Step 3

--- lib/utils/app_colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color backgroundBeige = Color(0xFFF5F5F5); 
-  static const Color cardDark = Color(0xFF2C2A29); 
-  static const Color textPrimary = Color(0xFF1A1A1A);
-  static const Color textSecondary = Color(0xFF757575);
-  static const Color buttonText = Colors.white;
-}
